d527b28d31f18dc19b19309597931a060a6114cd
Krishna Haranath <haranathpriyan@gmail.com>
2021-03-18 19:03:22 +0530

    modified home page

--- include/form.php
+++ include/form.php
@@ -27,8 +27,8 @@ require 'phpmailer/src/SMTP.php';
 $toemails = array();
 
 $toemails[] = array(
-				'email' => 'your-email@website.com', // Your Email Address
-				'name' => 'Your Name' // Your Name
+				'email' => 'hr@infocuscs.com', // Your Email Address
+				'name' => 'Infocus HR' // Your Name
 			);
 
 
@@ -37,8 +37,8 @@ $toemails[] = array(
 ---------------------------------------------------*/
 
 $fromemail = array(
-				'email' => 'no-reply@website.com', // Company's Email Address (preferably currently used Domain Name)
-				'name' => 'Company Name' // Company Name
+				'email' => 'hr@infocuscs.com', // Company's Email Address (preferably currently used Domain Name)
+				'name' => 'Infocus Consulting Services' // Company Name
 			);
 
 
